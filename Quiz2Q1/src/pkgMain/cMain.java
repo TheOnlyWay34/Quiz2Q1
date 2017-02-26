3617db89b7d8a18e26621176715b86567df54457
Matthew <Matthew@MattJsPc.fios-router.home>
2017-02-25 19:30:46 -0500

    Second Commit
    Have fully functioning code, just needs additional testing to make sure
    it is the final product

--- Quiz2Q1/src/pkgMain/cMain.java
+++ Quiz2Q1/src/pkgMain/cMain.java
@@ -1,10 +1,24 @@
 package pkgMain;
 
+import java.util.Scanner;
+
 public class cMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner scan;
 
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		System.out.println("Please input tuition cost for first year: ");
+		double tuition = scan.nextDouble();
+		double total = tuition;
+		double interest = 0;
+		for (double year = 2;year<5;year += 1){
+			System.out.printf("Please input tuition percentage increase for year %.0f: \n",year);
+			interest = scan.nextDouble();
+			tuition *= ((interest/100)+1);
+			total += tuition;
+		}
+		System.out.printf("Total cost over four years is $%.2f",total);
 	}
 
 }
